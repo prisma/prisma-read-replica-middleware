28af8166bca58b83e198643945e10c6118b18e33
Andrew Carlson <5479270+andrewicarlson@users.noreply.github.com>
2022-02-25 15:56:06 -0600

    Initial working PoC. _very messy_

---
+++ src/middleware/PrismaReadReplica.ts
@@ -0,0 +1,62 @@
+import { Prisma, PrismaClient } from "../../prisma/dup-client";
+
+const prisma2 = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error']
+});
+
+const lowercaseFirstLetter = (string: string) => {
+    return string.charAt(0).toLowerCase() + string.slice(1)
+}
+
+/**
+ *
+ * @param modelsToExclude
+ * @constructor
+ *
+ * Requirements:
+ * 1. User must be able to exclude models from the read-replica
+ * 2. The middleware must only intercept requests with models and find* actions
+ * 3. The user must be able to provide a RR datasource
+ * 4. The user must be able to provide a RR client provider
+ */
+
+const PrismaReadReplica = (modelsToExclude: string[] = [], client: any) => {
+    const middleware = async (
+        params: Prisma.MiddlewareParams,
+        next: (params: Prisma.MiddlewareParams) => Promise<any>
+    ) => {
+        const actionsToIntercept = [
+            "find",
+            "findMany",
+            "findUnique"
+        ];
+
+        if (params.model === undefined) {
+            return await next(params);
+        }
+
+        const shouldIgnoreAction = actionsToIntercept.indexOf(params.action) < 0;
+
+        if (shouldIgnoreAction) {
+            return await next(params);
+        }
+
+        const shouldIgnoreModel = modelsToExclude.indexOf(params.model) > -1;
+
+        if (shouldIgnoreModel) {
+            return await next(params);
+        }
+
+        // We need to call next() or all subsequent middleware is skipped. See https://www.prisma.io/docs/concepts/components/prisma-client/middleware#running-order-and-the-middleware-stack
+        await next(params);
+
+        // @ts-ignore
+        return prisma2[lowercaseFirstLetter(params.model)][params.action](params.args)
+    }
+
+    return {
+        middleware
+    }
+}
+
+export default PrismaReadReplica;
