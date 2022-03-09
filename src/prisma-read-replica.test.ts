1c5cb360934425ae46190206a96c71938cf128f3
Andrew Carlson <5479270+andrewicarlson@users.noreply.github.com>
2022-03-09 14:47:32 -0600

    Ran prettier

--- src/prisma-read-replica.test.ts
+++ src/prisma-read-replica.test.ts
@@ -1,62 +1,101 @@
 import PrismaReadReplica from ".";
 
-jest.mock('../prisma/read-replica-client', () => {
-    return {
-        PrismaClient: jest.fn(() => {
-            return {
-                user: {
-                    findMany: jest.fn(() => {
-                        return 'function hit'
-                    })
-                }
-            }
-        })
-    }
+jest.mock("../prisma/read-replica-client", () => {
+  return {
+    PrismaClient: jest.fn(() => {
+      return {
+        user: {
+          findMany: jest.fn(() => {
+            return "function hit";
+          }),
+        },
+      };
+    }),
+  };
 });
 
 test("PrismaReadReplica returns a middleware function", () => {
-    expect(PrismaReadReplica()).toHaveProperty('middleware');
+  expect(PrismaReadReplica()).toHaveProperty("middleware");
 });
 
 test("PrismaReadReplica calls next if the model is undefined", async () => {
-    const nextMock = jest.fn();
-    const middleware = PrismaReadReplica().middleware({ action: "count", args: '', dataPath: [], runInTransaction: false}, nextMock)
+  const nextMock = jest.fn();
+  const middleware = PrismaReadReplica().middleware(
+    { action: "count", args: "", dataPath: [], runInTransaction: false },
+    nextMock
+  );
 
-    expect(nextMock).toBeCalled();
+  expect(nextMock).toBeCalled();
 });
 
 test("PrismaReadReplica calls next if the action called should be ignored", async () => {
-    const nextMock = jest.fn();
+  const nextMock = jest.fn();
 
-    // @ts-ignore
-    const middleware = PrismaReadReplica().middleware({ action: "create", model: "", args: '', dataPath: [], runInTransaction: false}, nextMock)
+  // @ts-ignore
+  const middleware = PrismaReadReplica().middleware(
+    {
+      action: "create",
+      model: "",
+      args: "",
+      dataPath: [],
+      runInTransaction: false,
+    },
+    nextMock
+  );
 
-    expect(nextMock).toBeCalled();
+  expect(nextMock).toBeCalled();
 });
 
 test("PrismaReadReplica calls next if the model called should be ignored", async () => {
-    const nextMock = jest.fn();
-    
-    // @ts-ignore
-    const middleware = PrismaReadReplica(["User"]).middleware({ action: "find", model: "User", args: '', dataPath: [], runInTransaction: false}, nextMock)
+  const nextMock = jest.fn();
 
-    expect(nextMock).toBeCalled();
+  // @ts-ignore
+  const middleware = PrismaReadReplica(["User"]).middleware(
+    {
+      action: "find",
+      model: "User",
+      args: "",
+      dataPath: [],
+      runInTransaction: false,
+    },
+    nextMock
+  );
+
+  expect(nextMock).toBeCalled();
 });
 
 test("PrismaReadReplica calls new prisma client if other checks pass", async () => {
-    const nextMock = jest.fn();
-    
-    // @ts-ignore
-    const middleware = PrismaReadReplica(["User"]).middleware({ action: "find", model: "User", args: '', dataPath: [], runInTransaction: false}, nextMock)
+  const nextMock = jest.fn();
 
-    expect(nextMock).toBeCalled();
+  // @ts-ignore
+  const middleware = PrismaReadReplica(["User"]).middleware(
+    {
+      action: "find",
+      model: "User",
+      args: "",
+      dataPath: [],
+      runInTransaction: false,
+    },
+    nextMock
+  );
+
+  expect(nextMock).toBeCalled();
 });
 
 test("PrismaReadReplica calls new prisma client if other checks pass", async () => {
-    const nextMock = jest.fn();
-    
-    // @ts-ignore
-    const middleware = await PrismaReadReplica([]).middleware({ action: "findMany", model: "User", args: '', dataPath: [], runInTransaction: false}, nextMock)
+  const nextMock = jest.fn();
 
-    expect(middleware).toEqual('function hit');
+  // @ts-ignore
+  const middleware = await PrismaReadReplica([]).middleware(
+    {
+      action: "findMany",
+      model: "User",
+      args: "",
+      dataPath: [],
+      runInTransaction: false,
+    },
+    nextMock
+  );
+
+  expect(middleware).toEqual("function hit");
 });
